3b23a0e48aece1fef3125438b2ba335a9372ee92
pablo17sanchez <pablo17sanchez@hotmail.com>
2015-01-15 11:36:51 -0400

    ACTU
    ACTU

---
+++ consola/EncryptarYdesencrytar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consola
+{
+    class EncryptarYdesencrytar
+    {
+
+        public  string Encriptar( string _cadenaAencriptar)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+        public  string DesEncriptar( string _cadenaAdesencriptar)
+        {
+            string result = string.Empty;
+            byte[] decryted = Convert.FromBase64String(_cadenaAdesencriptar);
+            //result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+            result = System.Text.Encoding.Unicode.GetString(decryted);
+            return result;
+        }
+    }
+}
---
+++ consola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DETECT_OPERATION_STATE op = new DETECT_OPERATION_STATE();
+          
+            
+            Console.ReadKey();  
+        }
+    }
+}
